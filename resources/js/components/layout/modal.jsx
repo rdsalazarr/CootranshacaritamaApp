91c4cc9ff44340441e6b1fb7afe68da36bc6c27c
Ramon Salazar <rdsalazarr@ufpso.edu.co>
2024-01-30 16:29:10 -0500

    30-01-2024

--- resources/js/components/layout/modal.jsx
+++ resources/js/components/layout/modal.jsx
@@ -1,28 +1,7 @@
 
 import React, {useState , useEffect} from 'react';
-import Card from '@mui/material/Card';
-import { Box, CardContent, Icon, Modal, Paper, Typography } from '@mui/material';
+import { Box, Modal, Paper, Typography } from '@mui/material';
 import '../../../scss/modal.scss';
-
-export function TabPanel(props) {
-    const {children, value, index, ...other} = props;
-
-    return (
-        <Box
-            role="tabpanel"
-            hidden={value !== index}
-            id={`scrollable-prevent-tabpanel-${index}`}
-            aria-labelledby={`scrollable-prevent-tab-${index}`}
-            {...other}
-        >
-            <Card> 
-                <CardContent>
-                    {value === index && ( children)}     
-                </CardContent>
-            </Card>
-        </Box>
-    );
-}
 
 export function ModalDefault({title, content, close, tam = 'bigFlot'}){
     const [open , setOpen] = useState(true);
